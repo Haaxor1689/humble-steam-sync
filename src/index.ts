a4b5f92b7f92f5bbbd7698c7855a9fcc56e4a161
Haaxor1689 <betkomaros@gmail.com>
2021-12-26 10:16:40 +0100

    Endpoints updates

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import axios from 'axios';
+
 require('dotenv').config();
 
 type Item = { name: string };
-type GamesResponse = { data: { response: { games: { name: string }[] } } };
 
 const app = express();
 
@@ -27,31 +27,62 @@ const getOwnedGames = (steamId: string): Promise<Item[]> =>
     )
     .then(r => r.data.response?.games ?? []);
 
-app.get('/:steamId', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.humblebundle.com');
+const resolveVanityUrl = (name: string) =>
+  axios
+    .get(
+      `https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key=${process.env.STEAM_API_KEY}&vanityurl=${name}`
+    )
+    .then(r => r.data.response?.steamid ?? '');
 
-  Promise.all([
-    getWishlistPages(req.params.steamId),
-    getOwnedGames(req.params.steamId)
-  ])
-    .then(([wishlist, games]) => {
-      res.send([
-        ...wishlist.map(({ name }) => ({
-          name,
-          source: 'on wishlist'
-        })),
-        ...games.map(({ name }) => ({
-          name,
-          source: 'in library'
-        }))
-      ]);
-    })
-    .catch(e => {
-      res.send(e);
-    });
+const getUserProfile = (steamId: string) =>
+  axios
+    .get(
+      `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${process.env.STEAM_API_KEY}&steamids=${steamId}`
+    )
+    .then(r => r.data.response?.players?.[0]);
+
+app.get('/:steamId/games', async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.humblebundle.com');
+  try {
+    let steamId = req.params.steamId;
+    if (!steamId.match(/$\d+^/)) {
+      steamId = await resolveVanityUrl(steamId);
+    }
+
+    const [wishlist, games] = await Promise.all([
+      getWishlistPages(req.params.steamId),
+      getOwnedGames(req.params.steamId)
+    ]);
+    res.send([
+      ...wishlist.map(({ name }) => ({
+        name,
+        source: 'on wishlist'
+      })),
+      ...games.map(({ name }) => ({
+        name,
+        source: 'in library'
+      }))
+    ]);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+app.get('/:name/profile', async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  try {
+    let name = req.params.name;
+    if (!name.match(/$\d+^/)) {
+      name = await resolveVanityUrl(name);
+    }
+
+    res.send(await getUserProfile(name));
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 // start the Express server
 app.listen(process.env.PORT, () => {
-  console.log(`server started at http://localhost:${process.env.PORT}`);
+  console.log(`Server started at http://localhost:${process.env.PORT}`);
 });
