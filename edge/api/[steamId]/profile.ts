fb777f83d172ba23b45a9f1eaea4334b958f3bf3
Haaxor1689 <betkomaros@gmail.com>
2025-12-18 14:28:14 +0100

    Api update

--- edge/api/[steamId]/profile.ts
+++ edge/api/[steamId]/profile.ts
@@ -14,8 +14,6 @@ export default handler(async request => {
 	if (!response.ok) return new Response(JSON.stringify('null'));
 	const json = await response.json();
 
-	console.log('getUserProfile', json);
-
 	return new Response(
 		JSON.stringify({ steamId, avatar: json.response.players[0].avatarmedium })
 	);
